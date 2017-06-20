42b129c3f84fc1bf87ab10d1bd4852cfb2dcc245
charlesma4 <charlesma4@gmail.com>
2017-06-20 17:07:41 -0400

    prints all contacts from db!

--- src/main/java/com/teamtreehouse/contactmgr/Application.java
+++ src/main/java/com/teamtreehouse/contactmgr/Application.java
@@ -1,9 +1,15 @@
 package com.teamtreehouse.contactmgr;
 
+import com.teamtreehouse.contactmgr.model.Contact;
+import com.teamtreehouse.contactmgr.model.Contact.ContactBuilder;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.service.ServiceRegistry;
 
-import javax.imageio.spi.ServiceRegistry;
+import java.util.List;
 
 /**
  * Created by charl on 6/20/2017.
@@ -15,9 +21,53 @@ public class Application {
     private static SessionFactory buildSessionFactory() {
         //Create standard service registry object
         final ServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+        return new MetadataSources(registry).buildMetadata().buildSessionFactory();
     }
 
     public static void  main(String[] args) {
+        Contact contact = new ContactBuilder("Charles","Ma")
+                .withEmail("cma4@bu.edu")
+                .withPhone(8575403371L)
+                .build();
+        save(contact);
 
+        //Display list of contacts
+        for(Contact c: fetchAllContacts()) {
+            System.out.println(c);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private static List<Contact> fetchAllContacts() {
+        //Open session
+        Session session = sessionFactory.openSession();
+
+        //Create criteria
+        Criteria criteria = session.createCriteria(Contact.class);
+
+        //Store list of contact objects based on criteria
+        List<Contact> contacts = criteria.list();
+
+        //Close session
+        session.close();
+
+        return contacts;
+    }
+
+    public static void save(Contact contact) {
+        //Open session
+        Session session = sessionFactory.openSession();
+
+        //Begin transaction
+        session.beginTransaction();
+
+        //Use session to save contact
+        session.save(contact);
+
+        //Commit transaction
+        session.getTransaction().commit();
+
+        //Close session
+        session.close();
     }
 }
--- src/main/java/com/teamtreehouse/contactmgr/model/Contact.java
+++ src/main/java/com/teamtreehouse/contactmgr/model/Contact.java
@@ -24,7 +24,13 @@ public class Contact {
     @Column
     private Long phone;
 
-    public Contact(){
+    public Contact(){}
+
+    public Contact(ContactBuilder contactBuilder) {
+        this.firstName = contactBuilder.firstName;
+        this.lastName = contactBuilder.lastName;
+        this.email = contactBuilder.email;
+        this.phone = contactBuilder.phone;
     }
 
     @Override
@@ -77,4 +83,31 @@ public class Contact {
     public void setPhone(Long phone) {
         this.phone = phone;
     }
+
+    public static class ContactBuilder {
+        private String firstName;
+        private String lastName;
+        private String email;
+        private Long phone;
+
+        public ContactBuilder(String firstName, String lastName) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public ContactBuilder withEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public ContactBuilder withPhone(Long phone) {
+            this.phone = phone;
+            return this;
+        }
+
+        public Contact build() {
+            return new Contact(this);
+        }
+    }
+
 }
